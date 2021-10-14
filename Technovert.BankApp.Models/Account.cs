0f4a242f33ec0c56c3cadf9129185eb68e0ae475
Madhu Sri Sushmitha Chowdary <50200296+nekkantimadhu@users.noreply.github.com>
2021-10-14 22:00:16 +0530

    Models

---
+++ Technovert.BankApp.Models/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Technovert.BankApp.Models
+{
+    public class Account
+    {
+        //public string BankId { get; set;}
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public decimal Balance { get; set; }
+        public string Gender { get; set; } 
+        public string Mobile { get; set; }
+        public  AccountStatus Status { get; set; }
+       
+        public List<Transaction> TransactionHistory { get; set; } = new List<Transaction>();
+        public string CreatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ Technovert.BankApp.Models/Bank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Technovert.BankApp.Models
+{
+    public class Bank
+    {
+        public string BankId { get; set; }
+        public string Name { get; set; }
+        public List<Account> AccLists { get; set; } = new List<Account>();
+        public DateTime CreatedOn { get; set; }
+       
+    }
+}
