7f0e977baa6901c0355ffb1b476d54b482713a77
Madhu Sri Sushmitha Chowdary <50200296+nekkantimadhu@users.noreply.github.com>
2021-10-14 22:01:09 +0530

    Exceptions

---
+++ Technovert.BankApp.Models/Exceptions/AccNumFormatException.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technovert.BankApp.Models.Exceptions
+{
+    public static class AccNumFormatException
+    {
+        public static string Exception()
+        {
+            return "Invalid Account Number";
+        }
+    }
+}
