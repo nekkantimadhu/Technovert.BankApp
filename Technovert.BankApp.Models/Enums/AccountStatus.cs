ff751c4c87132884a163dc058a12a999045edd63
Madhu Sri Sushmitha Chowdary <50200296+nekkantimadhu@users.noreply.github.com>
2021-10-14 21:56:43 +0530

    Added Enums

---
+++ Technovert.BankApp.Models/Enums/AccountStatus.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Technovert.BankApp.Models
+{
+    public enum AccountStatus
+    {
+        Active,
+        PartiallyActive,
+        InActive,
+        Closed
+            
+       
+    }
+}
