dde7bd07fb77667b03fa5c85cbe6dd4e00c1a602
Madhu Sri Sushmitha Chowdary <50200296+nekkantimadhu@users.noreply.github.com>
2021-10-14 21:57:10 +0530

    Added Enums

---
+++ Technovert.BankApp.Models/Enums/TransactionType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technovert.BankApp.Models
+{
+    public enum TransactionType
+    {
+        Create = 0,
+        Deposit,
+        Withdraw,
+        Transfer,
+        Credit,
+        Debit
+    }
+
+}
