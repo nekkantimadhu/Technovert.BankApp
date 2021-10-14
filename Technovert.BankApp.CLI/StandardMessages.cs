b1c05ac24fd2a37166eecb7ca73c4e7d6d48a261
Madhu Sri Sushmitha Chowdary <50200296+nekkantimadhu@users.noreply.github.com>
2021-10-14 22:02:12 +0530

    CLI

---
+++ Technovert.BankApp.CLI/StandardMessages.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technovert.BankApp
+{
+    internal class StandardMessages
+    {
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Hello, Welcome!!");
+        }
+        public static void Options()
+        {
+            Console.WriteLine("1.Create Account\n2.Transaction Type\n3.Transaction History\n4.Exit");
+            Console.WriteLine("Enter your required option");
+        }
+        public static void TransactionOptions()
+        {
+            Console.WriteLine("1.Deposit\n2.Withdraw\n3.Transfer");
+            Console.WriteLine("Enter your required option");
+        }
+        public static void AmountInformation(string s)
+        {
+            Console.WriteLine($"Enter amount you want to { s }");
+        }
+        public static void InavlidInformation()
+        {
+            Console.WriteLine("Entered wrong account details");
+        }
+        public static void ExitMessage()
+        {
+            Console.WriteLine("Thank you");
+        }
+        public static void Data(string s)
+        {
+            Console.WriteLine($"Enter { s } again");
+        }
+    }
+}
---
+++ Technovert.BankApp.CLI/TransactionTypesEnum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Technovert.BankApp.Models;
+using Technovert.BankApp.Services;
+
+namespace Technovert.BankApp.CLI
+{
+    public class TransactionTypesEnum
+    {
+        internal void TypeOfTransaction()
+        {
+            Console.WriteLine("Enter Transaction type");
+            StandardMessages.TransactionOptions();
+            TransactionType Option = (TransactionType)Enum.Parse(typeof(TransactionType), Console.ReadLine());
+            switch (Option)
+            {
+                case TransactionType.Deposit:
+                    DepositCLI depositCLI = new DepositCLI();
+                    depositCLI.deposit();
+                    break;
+                case TransactionType.Withdraw:
+                    WithdrawCLI withdrawCLI = new WithdrawCLI();
+                    withdrawCLI.withdraw();
+                    break;
+                case TransactionType.Transfer:
+                    TransferCLI transferCLI = new TransferCLI();
+                    transferCLI.transfer();
+                    break;
+            }
+        }
+    }
+}
